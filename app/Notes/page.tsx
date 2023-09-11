a6c60900e88727dd961756d93270a93466d2c84c
Rivenshell <rivenshell111@gmail.com>
2023-09-11 15:15:14 -0400

    repo update

--- app/Notes/page.tsx
+++ app/Notes/page.tsx
@@ -1,9 +1,12 @@
 import React from "react"
+import Nav from "../components/Nav"
+
 console.log("if this does")
 
 export default function page() {
   return (
     <>
+      <Nav />
       <div className="text-center">Is this working</div>
     </>
   )
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -231,13 +231,8 @@ export default function Example() {
                     href="#"
                     className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
                   >
-                    <img
-                      className="h-8 w-8 rounded-full bg-gray-800"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
                     <span className="sr-only">Your profile</span>
-                    <span aria-hidden="true">Tom Cook</span>
+                    <span aria-hidden="true">Admin</span>
                   </a>
                 </li>
               </ul>
