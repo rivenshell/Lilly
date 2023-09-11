2132d5c206aa45034100d1ff11a3b3cb5eb4d26a
Rivenshell <rivenshell111@gmail.com>
2023-09-10 21:09:47 -0400

    The

---
+++ app/Notes/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+console.log("if this does")
+
+export default function page() {
+  return (
+    <>
+      <div className="text-center">Is this working</div>
+    </>
+  )
+}
