51e1414c340599f17b1e82a49a59c2b78c8d9ff5
Rivenshell <rivenshell111@gmail.com>
2023-09-11 19:55:58 -0400

    repo update

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -43,8 +43,14 @@ const teams = [
     initial: "H",
     current: false,
   },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+  {
+    id: 2,
+    name: "Sami's Book Club👩🏾‍🏫",
+    href: "#",
+    initial: "T",
+    current: false,
+  },
+  { id: 3, name: "Rivenshell Corp", href: "#", initial: "W", current: false },
 ]
 
 function classNames(...classes: string[]) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Nav from "./components/Nav"
+import Calender from "./components/Calender"
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <Calender />
     </>
   )
 }
