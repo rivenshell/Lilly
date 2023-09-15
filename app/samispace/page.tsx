e4143b725411a1e3bc73ed3b94cb4dd879dc8e79
Rivenshell <rivenshell111@gmail.com>
2023-09-14 23:38:59 -0400

    repo update

--- app/samispace/page.tsx
+++ app/samispace/page.tsx
@@ -5,7 +5,7 @@ export default function samispace() {
   return (
     <>
       <Nav />
-      <div className="text-center text-lg">
+      <div className="text-center text-lg mx-3">
         <h2>Samispace</h2>
       </div>
     </>
