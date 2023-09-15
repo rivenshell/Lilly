ecea9900f344771064e868b6cbbc02a37b187533
Rivenshell <rivenshell111@gmail.com>
2023-09-15 18:02:47 -0400

    repo update

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -109,7 +109,7 @@ export default function Example() {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                   >
-                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                    <div className=" left-full top-0 flex w-16 justify-center pt-5">
                       <button
                         type="button"
                         className="-m-2.5 p-2.5"
