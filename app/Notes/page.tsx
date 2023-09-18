16620d1590a4a9ca9f5330ff0b38f4049179e8be
Rivenshell <rivenshell111@gmail.com>
2023-09-18 16:28:26 -0400

    repo update

--- app/Notes/page.tsx
+++ app/Notes/page.tsx
@@ -6,6 +6,8 @@ console.log("if this does")
 export default function Notes() {
   return (
     <>
+      {/* Figue out the best solution for creating notes that save after user season (tied into the user account )
+        when user logs in the notes they commited to the project should remain  */}
       <Nav />
       <div className="text-center">
         😏
--- app/reflection/page.tsx
+++ app/reflection/page.tsx
@@ -5,6 +5,8 @@ import AppShell from "../components/AppShell"
 export default function reflection() {
   return (
     <>
+      {/* same concep as the notes app with user based contribution that tesets upon login. 
+          Primary page will be a login page. I can buid a site by reading docs. im not a big  */}
       <Nav />
       <div className="text-center text-lg">
         <h2>Reflection</h2>
