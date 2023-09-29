0c79c39ea8e6fbb3ff04f3993cd97263d9a8ae7b
Rivenshell <rivenshell111@gmail.com>
2023-09-29 19:48:46 -0400

    repo update

--- app/Notes/page.tsx
+++ app/Notes/page.tsx
@@ -9,14 +9,16 @@ console.log(n)
 export default function Notes() {
   return (
     <>
-      {/* Figue out the best solution for creating notes that save after user season (tied into the user account )
+      <div className="flex flex-row">
+        {/* Figue out the best solution for creating notes that save after user season (tied into the user account )
         when user logs in the notes they commited to the project should remain  */}
-      <Nav />
-      <div className="text-center">
-        😏
-        <h2 className="text-2xl">Notes</h2>
+        <Nav />
+        <div className="text-center">
+          😏
+          <h2 className="text-2xl">Notes</h2>
+        </div>
+        <Payment />
       </div>
-      <Payment />
     </>
   )
 }
